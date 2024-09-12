8d3ec108cfc8a65ff477a97270ca99929bc9f4ed
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-09-12 00:09:43 -0300

    challenge complete

--- 557/Program.cs
+++ 557/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System.Text;
+
+namespace FiveFiveSeven;
+
+class Program
+{
+    private const string S = "text for example";
+
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine(ReverseWords(S));
+        
+    }
+
+    static string ReverseWords(string s)
+    {
+        var words = s.Split(' ');
+
+        var resultString = new StringBuilder();
+
+        foreach (var word in words)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                resultString.Append(word[word.Length - i - 1]);
+            }
+            
+            resultString.Append(' ');
+        }
+        
+        return resultString.ToString().TrimEnd();
+    }
+}

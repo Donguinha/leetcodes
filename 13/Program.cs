75dd659a463eb87a5dd055ebb892c04ca93e4e63
Guilherme Rodrigues <oliveirack1@gmail.com>
2024-10-13 22:16:16 -0300

    leetcode 13

--- 13/Program.cs
+++ 13/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string roman = "CDLXXVI";
+
+RomanToInt(roman);
+
+public int RomanToInt(string s)
+{
+    int roman = 0;
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (s[i] == 'C')
+            if (s[i + 1] == '')
+
+
+
+    }
+}
+
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
